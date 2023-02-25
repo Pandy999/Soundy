3e0e015242756d59060fe131971197acc2d08dba
Paillat <git@electronique.cc>
2023-02-25 19:19:36 +0100

    moved database to /data

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from chat3 import chatgpt_response3
 import sqlite3
 from discord import default_permissions
 
-conn = sqlite3.connect('soundy.db')
+conn = sqlite3.connect('./data/soundy.db')
 c = conn.cursor() # create a cursor
 # create a table with the following values guild id, musical channel, bully channel, wise channel, general channel
 c.execute('''CREATE TABLE IF NOT EXISTS soundy (guild_id text, musical_channel integer, bully_channel integer, wise_channel integer, welcome_channel integer, api_key text)''')
