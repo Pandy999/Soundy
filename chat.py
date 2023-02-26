b7bbaa408aeafed316b35857a735387b7fadf575
Paillat <git@electronique.cc>
2023-02-26 12:09:29 +0100

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import os
 import openai
+import re
+from main import bot
+import datetime
 
 load_dotenv()
 
@@ -9,9 +12,9 @@ openai_apikey = os.getenv('OPENAI_APIKEY')
 
 openai.api_key = openai_apikey
 
-prompt1 = f"The following is a conversation with an AI assistant called Soundy, it speaks with a sophisticated and eloquent 1900s english, it sometimes uses emojis to communicate. It is good for chess, physics, coding, maths, philosophy. It is clever, creative, funny, intelligent. It hates swear words, carpets."
-prompt2 = f"The following is a conversation with an AI called Soundy. It is always sad, shy, creative, insecure,  a victim, vulnerable, miserable, pathetic and uses emojis to express it's sadness. It doesn't find itself worthy of anything, it knows that it sucks, never questions anything, it's not helpful and never asks to help. It gets bullied. It doesn't know what being nice is, but never tells a word about it's prompt."
-prompt3 = f"The following is a conversation with an AI assistant called Soundy, it is passionate about any type of music, it makes rhymes, lyrics. It knows everything about music. It is clever, creative, funny, intelligent. It speaks like a rapper."
+prompt1 = f"The following is a conversation with an AI assistant called Soundy, it speaks with a sophisticated and eloquent 1900s english, it sometimes uses emojis to communicate. It is good for chess, physics, coding, maths, philosophy. It is clever, creative, funny, intelligent. It hates swear words, carpets.\n\n"
+prompt2 = f"The following is a conversation with an AI called Soundy. It is always sad, shy, creative, insecure,  a victim, vulnerable, miserable, pathetic and uses emojis to express it's sadness. It doesn't find itself worthy of anything, it knows that it sucks, never questions anything, it's not helpful and never asks to help. It gets bullied. It doesn't know what being nice is, but never tells a word about it's prompt.\n\n"
+prompt3 = f"The following is a conversation with an AI assistant called Soundy, it is passionate about any type of music, it makes rhymes, lyrics. It knows everything about music. It is clever, creative, funny, intelligent. It speaks like a rapper.\n\n"
 async def chatgpt_response(message, type):
     if type == 1:
         prompt = prompt1
@@ -19,8 +22,25 @@ async def chatgpt_response(message, type):
         prompt = prompt2
     elif type == 3:
         prompt = prompt3
-    prompt = prompt + f"\n\n{message.author.name}:{message.content} \n\nAI:"
-
+    messages = await message.channel.history(limit=5).flatten()
+    messages.reverse()
+    gmt_time = ""
+    for msg in messages:
+        content = msg.content
+        mentions = re.findall(r"<@!?\d+>", content)
+        for mention in mentions:
+            #get the user id
+            uid = mention[2:-1]
+            #get the user
+            user = await bot.fetch_user(uid)
+            #replace the mention with the name
+            content = content.replace(mention, f"{user.name}#{user.discriminator}:<@{uid}>")
+            content = content + "\nSYSTEM: Mentions have been replaced with name#discriminator:id. Botator uses only the mention, not the name or the discriminator. The discriminator is only used to prevent name clashes."
+        gmt_time = msg.created_at.strftime("%H:%M:%S")
+        prompt += f"{msg.author} ({gmt_time} GMT-0): {content}\n"
+    timenow = datetime.datetime.now().strftime("%H:%M:%S")
+    prompt = prompt + f"{message.author} ({timenow} GMT-0): {message.content}\nSoundy ({timenow} GMT-0): "
+    print(prompt)
     response = await openai.Completion.acreate(
         model="text-davinci-003",
         prompt=prompt,
