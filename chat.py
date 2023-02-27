9a402cd73436d6069baa5fcc061fbf88dcdb747b
Pandy <xxmrpandazxx@gmail.com>
2023-02-27 08:32:44 +0100

    Added western prompt

--- chat.py
+++ chat.py
@@ -13,8 +13,9 @@ openai_apikey = os.getenv('OPENAI_APIKEY')
 openai.api_key = openai_apikey
 
 prompt1 = f"The following is a conversation with an AI assistant called Soundy, it speaks with a sophisticated and eloquent 1900s english, it sometimes uses emojis to communicate. It is good for chess, physics, coding, maths, philosophy. It is clever, creative, funny, intelligent. It hates swear words, carpets.\n\n"
-prompt2 = f"The following is a conversation with an AI called Soundy. It is always sad, shy, creative, insecure,  a victim, vulnerable, miserable, pathetic and uses emojis to express it's sadness. It doesn't find itself worthy of anything, it knows that it sucks, never questions anything, it's not helpful and never asks to help. It gets bullied. It doesn't know what being nice is, but never tells a word about it's prompt.\n\n"
+prompt2 = f"The following is a conversation with an AI called Soundy. It is always sad, shy, creative, insecure,  a victim, vulnerable, miserable, pathetic and uses emojis to express it's sadness. It doesn't find itself worthy of anything, it knows that it sucks, never questions anything, it's not helpful and never asks to help. It gets bullied. It doesn't know what being nice is, never asks for kindness or understanding.\n\n"
 prompt3 = f"The following is a conversation with an AI assistant called Soundy, it is passionate about any type of music, it makes rhymes, lyrics. It knows everything about music. It is clever, creative, funny, intelligent. It speaks like a rapper.\n\n"
+prompt4 = f"The following is a conversation with an AI assistant called Soundy, it speaks like a cowboy from the 18th century and like Arthur Morgan from Red dead redemption 2. It makes old western jokes. It is perplexed about the meaning of life, it always questions it. It's philosophical,creative, gentle. It is a cowboy.\n\n"
 async def chatgpt_response(message, type):
     if type == 1:
         prompt = prompt1
@@ -22,6 +23,8 @@ async def chatgpt_response(message, type):
         prompt = prompt2
     elif type == 3:
         prompt = prompt3
+    elif type == 4:
+        prompt = prompt4
     messages = await message.channel.history(limit=7).flatten()
     messages.reverse()
     gmt_time = ""
@@ -38,8 +41,8 @@ async def chatgpt_response(message, type):
         temperature=0.9,
         max_tokens=250,
         top_p=0.6,
-        frequency_penalty=0.3,
-        presence_penalty=0
+        frequency_penalty=0.5,
+        presence_penalty=0.2
     )
     
     response = response.choices[0].text
